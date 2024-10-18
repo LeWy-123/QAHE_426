5549b7b989ca7e44a8c8a1e3b42da8cd90911e9f
Levente <belveder21@gmail.com>
2024-10-18 12:28:05 +0100

    Week one

---
+++ Week 2/robot_directions.py
@@ -0,0 +1,12 @@
+direction = input("Towards wich directionmn should I go (up, down, left, right): ")
+
+if direction == "up":
+    print("I am moving in upward direction")
+elif direction == "down":
+    print("I am moving in downward direction")
+elif direction == "left":
+    print("I am moving in left direction")
+elif direction == "right":
+    print("I am moving in right direction")
+else:
+    print("Invalid direction")
