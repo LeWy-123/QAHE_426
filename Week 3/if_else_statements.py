2aba6b990f47b22509aba3a829218c978f6a1a8f
Levente <belveder21@gmail.com>
2024-10-22 11:06:05 +0100

    nope 2

--- Week 3/if_else_statements.py
+++ Week 3/if_else_statements.py
@@ -13,7 +13,7 @@ elif user_input == 'quit':
 else:
     print(f'What did you type man? :D ({user_input}), that\'s not included! in the program')
 
-
+print('-----------------------------')
 # Task 2 in week 2
 print('Enter the activity to be performed: ')
 user_input = input('Answer> ').lower()
@@ -26,5 +26,21 @@ if user_input == 'calculate' or 'calc' or 'calculation':
         print('It wasn\'t valid mathematical')
 
 print('Activity completed!')
-
+print('-----------------------------')
 # Task 3 week 2
+print('Please enter whole number: ')
+try:
+    user_input = int(input('→ '))
+    if user_input % 2 == 0:
+        print(f'your number is even my friend: {user_input}')
+    elif user_input % 2 != 0:
+        print(f'your number is odd my friend: {user_input}')
+    else:
+        print('It was not what i am expecting')
+except ValueError:
+    print('It was not integer may you type a text?')
+finally:
+    print('activity completed! my friend')
+
+print('-----------------------------')
+#task 4
