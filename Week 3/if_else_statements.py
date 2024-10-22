81158ab6e303a4521803143213200db072cd7df5
Levente <belveder21@gmail.com>
2024-10-22 10:41:39 +0100

    nope 2

--- Week 3/if_else_statements.py
+++ Week 3/if_else_statements.py
@@ -1,3 +1,4 @@
+# Task 1 in week 2 > :D
 print('Would you like to read books? ', end='>> ')
 user_input = input().lower()
 
@@ -11,3 +12,17 @@ elif user_input == 'quit':
     print('Thank you for playing!')
 else:
     print(f'What did you type man? :D ({user_input}), that\'s not included! in the program')
+
+
+# Taks 2 in week 2
+print('Enter the activity to be performed: ')
+user_input = input('Answer> ').lower()
+if user_input == 'calculate' or 'calc' or 'calculation':
+    print('Calculating...')
+    user_input = input('math equation> ').lower()
+    try:
+        print('your result: ', eval(user_input))
+    except:
+        print('It wasn\'t valid mathematical')
+
+print('Activity completed!')
