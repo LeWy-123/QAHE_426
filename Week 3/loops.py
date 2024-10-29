ed795a5bd012da5b2b89b1c3ac6d70331f513236
Levente <belveder21@gmail.com>
2024-10-29 12:07:01 +0000

    Added code to demonstrate the use of a while loop with len function

--- Week 3/loops.py
+++ Week 3/loops.py
@@ -26,13 +26,22 @@ print('How many bars should be charged?')    # prompt the user fpr desired charg
 charge_level = 1
 desired_level = int(input("your answer> "))  # the actual prompt where assign to a variable
 while charge_level <= desired_level:
-    if charge_level == 0:
+    if charge_level == 0:                    # this i put here if the charge level is 0 it will displays it
         print(f'Battery empty')
     else:
         print(f'Charging {bar * charge_level}')
-    charge_level += 1
+    charge_level += 1                        # increment charge bars by 1
 
 print('\nThe battery is fully charged.')
 print()
 
-#
+# Activity 4: Repeating Word
+parse = input('Please enter a phrase: ')     # in prompts the user for a string
+parse = parse.replace(" ", "")   # removes any empty spaces just to count the text and not all objects
+i = 0
+while i < len(parse):                        # while loop runs till reaches the length of the parse
+    print('Hi ', end='')
+    i += 1
+print()
+
+# Activity 5: Sum 100
