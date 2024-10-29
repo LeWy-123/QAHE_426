08cf0d422e30320686a29c460c5ae32eea2cd722
Levente <belveder21@gmail.com>
2024-10-29 11:57:07 +0000

    Added code to demonstrate the use of a while loop with ASCII art.

--- Week 3/loops.py
+++ Week 3/loops.py
@@ -6,14 +6,33 @@ while apples > 0:                            # opens a loop that counts backward
     print(f'apple removed! {apples} left')   # prints Apple removed every line and tells how many apples left
     apples -= 1                              # decrement the variable when reach 0 it breaks the loop
 
+print()
+
 # Activity 2: Count
-print('How many obstacles must I avoid?')
+print('How many obstacles must I avoid?')    # print the question
 avoidant = int(input("your answer> "))
 obstacles = 0
-while avoidant > obstacles:
+while avoidant > obstacles:                  # opens a while loop to count and rach the desired obstacle numbers
     print('Avoiding...', end = '')
     obstacles += 1
     print(f'...Done! {obstacles} obstacles avoided!')
 
 print('\nAll obstacles have been avoided.')
+print()
 
+# Activity 3: ASCII
+bar = '█'                                    # crete a variable contains the ascii character
+print('How many bars should be charged?')    # prompt the user fpr desired charging level
+charge_level = 1
+desired_level = int(input("your answer> "))  # the actual prompt where assign to a variable
+while charge_level <= desired_level:
+    if charge_level == 0:
+        print(f'Battery empty')
+    else:
+        print(f'Charging {bar * charge_level}')
+    charge_level += 1
+
+print('\nThe battery is fully charged.')
+print()
+
+#
