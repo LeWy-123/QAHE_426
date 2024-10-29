d2b19c4d538787a96fe299dcda6d0b749baab0d7
Levente <belveder21@gmail.com>
2024-10-29 12:39:59 +0000

    Added code to demonstrate the use of a while loop in performing calculations with userinput.

--- Week 3/loops.py
+++ Week 3/loops.py
@@ -45,3 +45,31 @@ while i < len(parse):                        # while loop runs till reaches the 
 print()
 
 # Activity 5: Sum 100
+print('Calculating the sum of the first 100 numbers using the sum method...')
+hundred_numbers = [n for n in range(1, 101)] # it creates a list with numbers up to 100 from 1. I used list comprehension
+print(sum(hundred_numbers))                  # use a sum built-in function to print the sum
+print()
+i = 1
+
+# using the while loop method
+print('Calculating the sum of the first 100 numbers...')
+answer = 1                                   # variable that will store the numbers and we do the calculations
+while i < 100:                               # this is a while loop runs till reaches 100 after stop running
+    answer += i + 1                          # here the answer variable will be added i and 1. this way the numbers are
+    print(answer)                            # always ahead by 1 what we add to the answer variable
+    i += 1
+
+print(f'...Done! The answer is {answer}')    # the final output should be 5050 but can be changed to add up any numbers
+
+# Activity 6: Sum User Numbers
+print('How many numbers should I sum up?')
+counting_up = int(input("your answer> "))
+i = 1
+calc = 0
+while i < counting_up:
+    print(f'Please enter a number {i} of {counting_up}:')
+    summable_number = int(input("your answer> "))
+    calc += summable_number
+    i += 1
+
+print(f'...Done! The answer is {calc}')
