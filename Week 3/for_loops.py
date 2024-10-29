7264642bbce73e4c5622f2dbdd23323aed61eacf
Levente <belveder21@gmail.com>
2024-10-29 13:41:53 +0000

    Added code to demonstrate the use of a simple for loop.

--- Week 3/for_loops.py
+++ Week 3/for_loops.py
@@ -2,15 +2,36 @@
 print('How many mountains should I display?')
 mountains = int(input('amount> '))
 i = 0
-mountain_ascii = """      __
+mountain_ascii = """      __ 
      /  \\_
     /^    \\
    / ^     \\_
  _/ ^ ^     ^\\
-/ ^       ^   \\"""
+/ ^       ^   \\"""                         # large ASCII art stored in a variable for cleaner code
+
 print('Displaying...')
 for i in range(mountains):
     print(mountain_ascii)
     i += 1
 
 print('Done!')
+
+# Activity 2: Count Down
+
+print('How far we from the target?')
+target = int(input('amount> '))             # promoting the user
+for i in range(target, 0, -1):              # for loop to count downwards
+    print(f'{i} steps remaining')           # printing to the terminal
+
+print('Target archived!')
+
+# Activity 3: Range
+print('what level of brightness is required?')
+brightness = int(input('amount> '))         # promoting the user for the brighness level
+
+
+print('\nAdjusting brightness...\n')
+
+for i in range(0, brightness, 2):
+    print(f'Brightness level: {"*"*i}')
+
