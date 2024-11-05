e966393ee217c18574f44f0f0f840a23ecb21d00
Levente <belveder21@gmail.com>
2024-11-05 11:39:45 +0000

    Added code to demonstrate the use of an if...elif...else statement.

--- Week 2/if_statements.py
+++ Week 2/if_statements.py
@@ -1,3 +1,4 @@
+# Activity 1
 def book_type_function():
     book_type = input("What type of book is this? ")
     if book_type == "adventure":
@@ -7,6 +8,7 @@ def book_type_function():
 
     print("Finished reading books")
 
+# Activity 2
 def activity_function():
     activity = input("Please enter activity to be performed: ")
     if activity == "calculate":
@@ -16,4 +18,21 @@ def activity_function():
 
     print("Activity completed")
 
-activity_function()
+# Activity 3
+def robot_directions():
+    # prompt the user for direction
+    direction = input("Towards which direction should I go (up, down, left, right): ")
+
+    # If statements for branching
+    if direction == "up":
+        print("I am moving in upward direction")
+    elif direction == "down":
+        print("I am moving in downward direction")
+    elif direction == "left":
+        print("I am moving in left direction")
+    elif direction == "right":
+        print("I am moving in right direction")
+    else:
+        print("Invalid direction")
+
+# Activity 4
