5924a2557b1aa54a1f41448b855097e1255cc6bc
Levente <belveder21@gmail.com>
2024-11-05 11:43:30 +0000

    Added code to demonstrate how a variable can be used as a counter.

--- Week 2/if_statements.py
+++ Week 2/if_statements.py
@@ -35,4 +35,43 @@ def robot_directions():
     else:
         print("Invalid direction")
 
-# Activity 4
+# Activity 4 Modulo Operator
+def odd_or_even():
+    number = int(input("Enter a number please: "))
+
+    if number % 2 == 0:
+        print(f"{number} was Even")
+    else:
+        print(f"{number} was Odd")
+
+# Activity 5: Comparison Operators
+def which_is_the_greater():
+    print("Please enter the first number please: ")
+    first_number = int(input("first number: >"))
+    print("Please enter the second number please: ")
+    second_number = int(input("second number: >"))
+
+    if first_number > second_number:
+        print(f"{first_number} was greatest!")
+    elif first_number < second_number:
+        print(f"{second_number} was greatest!")
+    else:
+        print(f"The two numbers were equal. {first_number} = {second_number}")
+
+# Activity 6: Counter
+def which_is_the_greater(loop_length=5, i = 0):
+    even_counter = 0
+    odd_counter = 0
+    while i < loop_length:
+        input_number = int(input(f"No: {i} - Enter a number: "))
+        if input_number % 2 == 0:
+            even_counter += 1
+            print("One even number added!")
+        else:
+            odd_counter += 1
+            print("One odd number added!")
+        i += 1
+    print("------------------------------------------------")
+    print("The number of even numbers is: ", even_counter)
+    print("The number of odd numbers is: ", odd_counter)
+    print("------------------------------------------------")
