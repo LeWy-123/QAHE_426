ff5ab0f109d72439a5a60d0b906e930b9d9e52e1
Levente <belveder21@gmail.com>
2024-11-05 13:43:51 +0000

    Added code to demonstrate the use of a user-defi ned function with a parameter.

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -16,4 +16,19 @@ def identify():
     else:
         print(f'We will be fi ne. coz it\'s {object_seen}.')
 
-# Activity 3:
+# Activity 3: Parameters
+def escape_by(plan=None):
+    if plan == 'jumping over':
+        print('We cannot escape that way! The boulder is too big!')
+    elif plan == 'running around':
+        print('We cannot escape that way! The boulder is moving too fast!')
+    elif plan == 'cross bridge ahead':
+        print('That might just work! Let\'s go!')
+    elif plan is None:
+        return 0
+    else:
+        print('We cannot escape that way! The boulder is in the way!')
+
+escape_by("jumping over")
+escape_by("running around")
+escape_by("cross bridge ahead")
