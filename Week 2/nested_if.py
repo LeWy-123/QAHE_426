b1748baa32edc3d88d4b3497020851b9a51912fe
Levente <belveder21@gmail.com>
2024-11-05 11:53:25 +0000

    Added code to demonstrate how a variable can be used as a counter.

--- Week 2/nested_if.py
+++ Week 2/nested_if.py
@@ -1,3 +1,4 @@
+# Activity 1: Nested Decision
 # Ask user for book details
 print("What type of cover does this book have?")
 cover_type = input ()
@@ -13,3 +14,39 @@ if cover_type == "soft":
         print("Soft covers with coils or stitches are great for short books")
 else:
     print("Books with hard covers can be more expensive!")
+
+# Activity 2: Multiple Nested Decisions
+# Start of the program
+def look_for_phone():
+    print("Where should I look?")
+    location = input()
+
+    if location == "in the bedroom":
+        print("Where in the bedroom should I look?")
+        specific_location = input()
+        if specific_location == "under the bed":
+            print("Found some shoes but no phone.")
+        else:
+            print("Found some mess but no phone.")
+
+    elif location == "in the bathroom":
+        print("Where in the bathroom should I look?")
+        specific_location = input()
+        if specific_location == "in the bathtub":
+            print("Found a rubber duck but no phone.")
+        else:
+            print("Found bathroom stuff but no phone.")
+
+    elif location == "in the living room":
+        print("Where in the living room should I look?")
+        specific_location = input()
+        if specific_location == "on the table":
+            print("Yes! I found my phone!")
+        else:
+            print("Found some stuff but no phone.")
+
+    else:
+        print("I do not know where that is but I will keep looking.")
+
+# Run the program
+look_for_phone()
