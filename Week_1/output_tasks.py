a5a26babb1fda431a4ded10ca661b7bf34eb19f4
Levente <belveder21@gmail.com>
2024-11-07 12:01:11 +0000

    Added code to display a multi-line message

--- Week_1/output_tasks.py
+++ Week_1/output_tasks.py
@@ -1,3 +1,8 @@
 # Activity 1: Outputting a Simple Message
 # Display message to the screen
 print("Welcome to QHO426!")
+
+# Activity 2: Outputting a Multi-line Message
+# Display message to the screen
+print("Welcome to QHO426!")
+print("In week 1 we will learn...")
