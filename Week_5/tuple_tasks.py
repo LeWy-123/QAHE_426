e8b6ef3f1d319dd9a60e492617e0df4272cf2db0
Levente <belveder21@gmail.com>
2024-11-20 18:25:45 +0000

    some fixes in code and tests

---
+++ Week_5/tuple_tasks.py
@@ -0,0 +1,17 @@
+# Activity 1: Simple Tuple
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods)
+
+def run_task1():
+    var = likelihood()
+    print(f'Minimum likelihood of falling: {var}%')
+
+# Activity 2: Tuple Return Type
+def likelihood_min_max():
+    
+
+
+
+if __name__ == '__main__':
+    run_task1()
